81e98d50924be4553abcf4477470e6bb90c2d462
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-04-24 20:57:13 +1200

    save changes in database

---
+++ CS_CODE/INITIAL/FilePathConsoleApp/FilePathConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace FilePathConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
+            Console.Read();
+        }
+    }
+}
