23cf713de7a7a37c0d591554430b438291a0b209
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-04-27 16:06:32 +1200

    create a new project for prototyping the app window layout

---
+++ CS_CODE/PROTOTYPING/MovieRentalWPF/MovieRentalWPF/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MovieRentalWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void OpenCustomerScreen(object sender, RoutedEventArgs e)
+        {
+            CustomerWindow window = new CustomerWindow();
+            window.Show();
+        }
+
+        private void IssueMovie(object sender, RoutedEventArgs e)
+        {
+            IssueMovieWindow window = new IssueMovieWindow();
+            window.Show();
+        }
+    }
+}
